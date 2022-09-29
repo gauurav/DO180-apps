381fed7f4fa93875776c499c7db2da64f364f2e7
Student User <student@workstation.lab.example.com>
2022-09-29 10:35:13 -0400

    hello chg

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -4,5 +4,12 @@ print "A change is a coming!\n";
 <<<<<<< HEAD
 =======
 print "Aarav & gauurav with vandy  change is a coming!\n";
+print "Aarav & gauurav with vandy  change is a coming!\n";
+print "Aarav & gauurav with vandy  change is a coming!\n";
+print "Aarav & gauurav with vandy  change is a coming!\n";
+print "Aarav & gauurav with vandy  change is a coming!\n";
+print "Aarav & gauurav with vandy  change is a coming!\n";
+print "Aarav & gauurav with vandy  change is a coming!\n";
+print "Aarav & gauurav with vandy  change is a coming!\n";
 >>>>>>> 7d3c5b3c07f51962b6a9c9d126ab5a9e39ecfbff
 ?>
